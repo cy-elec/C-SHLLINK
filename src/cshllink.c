c4fa5ddd44213b04ae27fd9b07720886d85175f2
Elec42 <70710128+Elec42@users.noreply.github.com>
2022-01-16 22:39:30 +0100

    name

--- src/cshllink.c
+++ src/cshllink.c
@@ -1,5 +1,5 @@
 /*
-    This library provides an interface for the Shell Link Binary File Format in C [MS-SHLLINK](https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-shllink/16cb4ca1-9339-4d0c-a68d-bf1d6cc0f943)
+    This library provides an interface for the MS Shell Link Binary File Format in C [MS-SHLLINK](https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-shllink/16cb4ca1-9339-4d0c-a68d-bf1d6cc0f943)
 
 
 
