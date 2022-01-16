d551108f26a6eba52eea8b31d476303bc8c28bee
Elec42 <70710128+Elec42@users.noreply.github.com>
2022-01-16 17:36:07 +0100

    added MAKEFILE, lib-files

---
+++ src/cshllink.c
@@ -0,0 +1,13 @@
+/*
+    This library provides an interface for the Shell Link Binary File Format in C [MS-SHLLINK](https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-shllink/16cb4ca1-9339-4d0c-a68d-bf1d6cc0f943)
+
+
+
+    Author: Felix Kröhnert
+    License: GNU GPL V3 (refer to LICENSE for more information)
+*/
+
+#include "cshllink.h"
+#include <stdio.h>
+
+
